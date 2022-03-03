5f13309aa8a03f193a602cfe67b501be229148b9
Your Name <you@example.com>
2022-03-03 13:04:36 +0100

    Validacion y contenedor inicial

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 Proyecto para desplegar clusters de openshift en OCI
+
+
+ssh-keygen -P "" -C "Usuario para Maquina Oracle" -t rsa -b 2048 -m pem -f ~/Oravm/orauser
+c
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "TenancyID" {
+  Type = string
+}
+variable "UserID" {
+  Type = string
+}
+variable "Region" {
+  Type = string
+}
+variable "CompartmentID" {
+  Type = string
+}
+variable "private_key_path"{
+  Type = string
+}
+
+variable "fingerprint"{
+  Type = string
+}
+variable "ssh_public_key" { 
+   Type = string
+}
+variable "ssh_private_key" { 
+    Type = string
+}
