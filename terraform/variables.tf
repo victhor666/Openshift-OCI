359eca12df00afe8bc35e214cf5c25afac4c38fd
Your Name <you@example.com>
2022-03-03 19:45:23 +0100

    cambio en variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,3 +34,19 @@ variable "vcn_core_cidr" {
 variable "vcn_core_dns_label" {
   type = string
 }
+
+#Network cluster
+variable "vcn_cluster_cidr" {
+  type = string
+}
+variable "vcn_cluster_dns_label" {
+  type = string
+}
+variable "vcn_cluster_display_name" {
+  type = string
+}
+
+#Subnet
+variable "cluster_subnet_cidr" {
+  type = string
+}
