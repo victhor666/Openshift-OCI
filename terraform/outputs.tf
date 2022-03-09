d54bcbeb94569e1ef959ff68fa53cf396a59682f
Your Name <you@example.com>
2022-03-09 08:41:33 +0100

    quitamos outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,45 +8,15 @@ output "compartimento-core-OCID" {
   value       = oci_identity_compartment.Core-Compartment.id
 }
 #outputs de red core
-    output "vcn_core_cidr" {
-      description = "CIDR de la red core "
-      value       = oci_core_vcn.Vcn-Core.cidr_block
-    }
-    output "vcn_core_Nombre" {
-      description = "Nombre de la red Core "
-      value       = oci_core_vcn.Vcn-Core.display_name
-    }
-    output "vcn_core_DominioDNS" {
-      description = "Nombre dominio "
-      value       = oci_core_vcn.Vcn-Core.vcn_domain_name
-    }
-  #outputs de red cluster
-    output "vcn_cluster_cidr" {
-      description = "CIDR de la red core "
-      value       = module.Cluster.cidr_vcn
-    }
-    output "vcn_cluster_Nombre" {
-      description = "Nombre de la red Core "
-      value       = module.Cluster.nombre_vcn
-    }
-    output "vcn_cluster_DominioDNS" {
-      description = "Nombre dominio cluster"
-      value       = module.Cluster.dominio_vcn
-    }
-     #outputs de Subred cluster
-        output "Subnet_cluster_cidr" {
-      description = "CIDR de la Subred core "
-      value       = module.Cluster.cidr_subnet
-    }
-    output "Subnet_cluster_Nombre" {
-      description = "Nombre de la Subred Core "
-      value       = module.Cluster.nombre_subnet
-    }
-    output "Subnet_cluster_DominioDNS" {
-      description = "Nombre dominio Subred cluster"
-      value       = module.Cluster.dominio_subnet
-    }
-       output "nombre_maquina_infra" {
-      description = "Nombre maquina de infra"
-      value       = module.Cluster.nombre_maquina_infra
-    }
+output "vcn_core_cidr" {
+  description = "CIDR de la red core "
+  value       = oci_core_vcn.Vcn-Core.cidr_block
+}
+output "vcn_core_Nombre" {
+  description = "Nombre de la red Core "
+  value       = oci_core_vcn.Vcn-Core.display_name
+}
+output "vcn_core_DominioDNS" {
+  description = "Nombre dominio "
+  value       = oci_core_vcn.Vcn-Core.vcn_domain_name
+}
