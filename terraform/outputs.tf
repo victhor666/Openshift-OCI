1d2317cad17fe3fc70587a423d467cd66906e8f6
Your Name <you@example.com>
2022-03-10 08:39:08 +0100

    outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,7 @@ output "Nombre-del-Compartimento-core" {
   description = "Compartimento para elementos core"
   value       = oci_identity_compartment.Core-Compartment.name
 }
-output "compartimento-core-OCID" {
-  description = "OCID del compartimento elementos core"
-  value       = oci_identity_compartment.Core-Compartment.id
-}
-#outputs de red core
+
 output "vcn_core_cidr" {
   description = "CIDR de la red core "
   value       = oci_core_vcn.Vcn-Core.cidr_block
@@ -19,4 +15,25 @@ output "vcn_core_Nombre" {
 output "vcn_core_DominioDNS" {
   description = "Nombre dominio "
   value       = oci_core_vcn.Vcn-Core.vcn_domain_name
+}
+#Compartimento cluster
+output "vcn_cluster" {
+  description = "Nombre dominio "
+  value       = oci_core_vcn.Vcn-Cluster.vcn_domain_name
+}
+output "vcn_cluster_cidr" {
+  description = "Nombre dominio "
+  value       = oci_core_vcn.Vcn-Cluster.cidr_block
+}
+output "Master-IP" {
+  description = "IP del master"
+  value       = oci_core_instance.Master-Instance.private_ip
+}
+output "Worker1-IP" {
+  description = "IP del master"
+  value       = oci_core_instance.Master-Instance2.private_ip
+}
+output "Worker2-IP" {
+  description = "IP del master"
+  value       = oci_core_instance.Worker-Instance2.private_ip
 }
