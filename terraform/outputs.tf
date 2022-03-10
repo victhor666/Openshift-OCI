f626c36709d53d3318b392a52b8323044fa1a028
Your Name <you@example.com>
2022-03-10 08:43:19 +0100

    typo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,13 +17,38 @@ output "vcn_core_DominioDNS" {
   value       = oci_core_vcn.Vcn-Core.vcn_domain_name
 }
 #Compartimento cluster
-output "vcn_cluster" {
-  description = "Nombre dominio "
-  value       = oci_core_vcn.Vcn-Cluster.vcn_domain_name
-}
-output "vcn_cluster_cidr" {
-  description = "Nombre dominio "
+
+output "cidr_vcn" {
+  description = "CIDR de la red Cluster "
   value       = oci_core_vcn.Vcn-Cluster.cidr_block
+}
+output "nombre_vcn" {
+  description = "Nombre de la red Cluster "
+  value       = oci_core_vcn.Vcn-Cluster.display_name
+}
+output "dominio_vcn" {
+  description = "Nombre dominio cluster"
+  value       = oci_core_vcn.Vcn-Cluster.dns_label
+}
+    output "cidr_subnet" {
+  description = "CIDR de la subred del  Cluster "
+  value       = oci_core_subnet.Cluster-Subnet.cidr_block
+}
+    output "dominio_subnet" {
+  description = "Nombre dominio dentro de la subnet"
+  value       = oci_core_subnet.Cluster-Subnet.dns_label
+}
+    output "nombre_subnet" {
+  description = "Nombre de la Subred "
+  value       = oci_core_subnet.Cluster-Subnet.display_name
+}
+  output "Nombre_maquina_Worker1" {
+  description = "Nombre maquina Worker1 "
+  value       = oci_core_instance.Worker-Instance1.display_name
+}
+  output "nombre_maquina_infra" {
+  description = "Nombre maquina Worker2 "
+  value       = oci_core_instance.Worker-Instance2.display_name
 }
 output "Master-IP" {
   description = "IP del master"
@@ -31,7 +56,7 @@ output "Master-IP" {
 }
 output "Worker1-IP" {
   description = "IP del master"
-  value       = oci_core_instance.Master-Instance2.private_ip
+  value       = oci_core_instance.Worker-Instance2.private_ip
 }
 output "Worker2-IP" {
   description = "IP del master"
